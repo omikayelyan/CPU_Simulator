0b8eed7a385246fed3bf341ed41bf011512387fe
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2022-09-25 22:18:42 +0400

    Update cpu.hpp

--- cpu.hpp
+++ cpu.hpp
@@ -17,7 +17,7 @@ CPU::CPU() : r0{}, r1{}, r2{}, r3{}, r4{}, r5{}, r6{}, r7{}, r8{}, r9{}
     cond[">"] = {"jge", "jg"};
     cond["<"] = {"jle", "jl"};
     cond["=="] = {"je", "jge", "jle"};
-    }
+}
 
 void CPU::translate()
 {
